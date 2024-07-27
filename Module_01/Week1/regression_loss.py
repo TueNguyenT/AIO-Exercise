cbe489cfb5c00aec24f44844fdcbf47636056bed
tuenguyent <nguyen.taitue.2901@gmail.com>
2024-07-28 03:30:28 +0700

    Add exercise module1 week1

---
+++ Module_01/Week1/regression_loss.py
@@ -0,0 +1,44 @@
+import math
+import random
+
+
+def calc_ae(y, y_hat):
+    return abs(y - y_hat)
+
+
+def calc_se(y, y_hat):
+    return (y - y_hat)**2
+
+
+def regression_loss():
+    num_samples = input(
+        'Input number of samples (integer number) which are generated: ')
+    if not num_samples.isnumeric():
+        print('number of samples must be an integer number')
+        return
+
+    loss_name = input('Input loss name: ')
+    final_loss = 0
+    num_samples = int(num_samples)
+
+    for i in range(num_samples):
+        predict = random.uniform(0, 10)
+        target = random.uniform(0, 10)
+
+        if loss_name == 'MAE':
+            loss = calc_ae(target, predict)
+        else:
+            loss = calc_se(target, predict)
+
+        print(
+            f'loss name: {loss_name}, sample: {i}, pred: {predict}, target: {target}, loss: {loss}')
+        final_loss += loss
+
+    final_loss = final_loss / num_samples
+    if loss_name == 'RMSE':
+        final_loss = math.sqrt(final_loss)
+    print(f'final {loss_name}: {final_loss}')
+
+
+if __name__ == "__main__":
+    regression_loss()
